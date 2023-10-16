bd86b7771449a58a87a55cb81e3e45e058f80087
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2023-10-16 19:36:41 +0530

    Time: 6 ms (72.2%), Space: 8.7 MB (90.01%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-int fact(int n)
-    {
-        int product=1;
-        for(int i=n;i>=1;i--)
-        {
-            product=product*i;
+vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> uniq_perms;
+        uniq_perms.push_back(nums);
+        while(next_permutation(nums.begin(), nums.end())){
+            uniq_perms.push_back(nums);
         }
-        return product;
-    }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-       vector<vector<int>>ans;
-       set<vector<int>>putting;
-        int n=nums.size();
-        int moves=fact(n);
-        for(int i=0;i<moves;i++)
-        {
-            putting.insert(nums);
-            next_permutation(nums.begin(),nums.end());
-        }
-        for(auto i:putting)
-        {
-            ans.push_back(i);
-        }
-        return ans;
+        return uniq_perms;
     }
 };
