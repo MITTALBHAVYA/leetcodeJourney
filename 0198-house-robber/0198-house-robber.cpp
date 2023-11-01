804f402fc1fb8b2e8abcc8fd6e70348194af2268
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2023-11-01 23:48:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.01%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev2=0;
+        int prev1=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            int ans=max(prev2+nums[i],prev1);
+            prev2=prev1;
+            prev1=ans;
+        }
+        return prev1;
+    }
+};
