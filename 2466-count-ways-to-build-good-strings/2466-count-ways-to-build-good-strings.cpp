cb29fee4049f5b6d6700ccab83bcbaf8e97586b1
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2023-11-20 23:45:10 +0530

    Time: 16 ms (19.98%), Space: 22 MB (17.88%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long helper(int length,int low,int high,int zero,int one,vector<long long>&dp){
+        if(length>high){
+            return 0;
+        }
+        if(dp[length]!=-1){return dp[length];}
+        long long ans=0;
+        if(length>=low){
+            ans=1;
+        }
+        ans+=helper(length+zero,low,high,zero,one,dp) + helper(length + one,low,high,zero,one,dp);
+        dp[length]=ans;
+        return ans%1000000007;
+    }
+    int countGoodStrings(int low, int high, int zero, int one) {
+         vector<long long>dp(high+1,-1);
+        return helper(0,low,high,zero,one,dp);
+    }
+};
